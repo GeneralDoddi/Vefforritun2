c6a40c6a44b90a662515b12caf5dc299f46679bb
Doddi <doddisti@gmail.com>
2014-01-16 22:12:23 +0000

    rectangle try 1

--- skilaverkefni 1/canvaspaint.js
+++ skilaverkefni 1/canvaspaint.js
@@ -1,26 +1,43 @@
-if(window.addEventListener) {
 
-	window.addEventListener('load', function () {
+
+//if(window.addEventListener) {
+
+	//window.addEventListener('load', function () {
 
 	var canvas;
 	var tool;
+	var tool_default = 'pencil'
 	var context;
+	var button;
 
 function init () {
-
-	
 
 		//penni
 	tool = new tool_pencil();
 		//canvasinn
 	canvas = document.getElementById('paint');
+
+	button = document.getElementById('rect');
 		//2d canvas context
 	context = canvas.getContext('2d');
 		//fylgist med eventum, ss mouse up, mouse down og mouse move
 	canvas.addEventListener('mousedown', ev_canvas, false);
 	canvas.addEventListener('mousemove', ev_canvas, false);
 	canvas.addEventListener('mouseup', ev_canvas, false);
+
+	//button.addEventListener('mouseclick', clicked, false)
 }
+
+function clicked(id) {
+	if(id === 'pencil') {
+		tool = new tool_pencil();
+	}
+	else if(id === 'rect') {
+		tool = new tool_rect();
+	}
+};
+
+
 
 function tool_pencil() {
 	var tool = this;
@@ -51,6 +68,38 @@ function tool_pencil() {
 	};
 }
 
+function tool_rect() {
+
+	var tool = this;
+	this.started = false;
+
+	this.mousedown = function (ev) {
+		tool.started = true;
+		tool.startX = ev.x;
+		tool.startY = ev.y;
+	};
+
+	this.mousemove = function(ev) {
+
+		var x = Math.min(ev.x, tool.startX);
+		var y = Math.min(ev.y, tool.startY);
+		var h = Math.abs(ev.x - tool.startX);
+		var w = Math.abs(ev.y - tool.startY);
+
+		context.strokeRect(x,y,h,w);
+	};
+
+	this.mouseup = function (ev) {
+      if (tool.started) {
+        tool.mousemove(ev);
+        tool.started = false;
+      }
+    };
+
+	//console.log("herro");
+
+};
+
 	function ev_canvas(ev) {
 		var x, y;
 
@@ -69,4 +118,4 @@ function tool_pencil() {
 	}
 
 	init();
-}, false) }
+//}, false) //}
