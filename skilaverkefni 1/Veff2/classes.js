7b9eb5a5817d2d2626e4618cc0278960edb9e123
bjarnthor <bjarnthor2408@gmail.com>
2014-01-29 19:56:33 +0000

    take a look at this

---
+++ skilaverkefni 1/Veff2/classes.js
@@ -0,0 +1,102 @@
+function Point(x,y){
+
+	this.x = x;
+	this.y = y;
+}
+//Pencil constructor and tools
+function Pencil () {
+	this.points = [];
+}
+
+Pencil.prototype.addPoint = function (point) {
+	this.points.push(point);
+}
+
+Pencil.prototype.draw = function(c) {
+	for (var i = 0; i < this.points.length; i++) {
+		
+		var current_point = this.points[i]
+		if(i == 0){
+			c.moveTo(current_point.x,current_point.y);
+		}
+		else{
+			c.lineTo(current_point.x,current_point.y);
+			c.stroke();
+		}
+	}
+}
+//Rectangle constrctor and tools
+function Rect (){
+	this.start = undefined;
+	this.end = undefined;
+}
+
+Rect.prototype.addPoint = function(point) {
+	if(this.start === undefined){
+		this.start = point;
+		console.log("Adding start point to rectangle");
+	}
+	else{
+		this.end = point;
+		console.log("Updating end point in rectangle");
+	}
+}
+
+Rect.prototype.draw = function(c) {
+	
+	var width = this.end.x - this.start.x;
+	var height = this.end.y - this.start.y;
+	c.rect(this.start.x, this.start.y, width, height);
+	c.stroke();
+	
+}
+
+function Line (){
+	this.start = undefined;
+	this.end = undefined; 
+}
+
+Line.prototype.addPoint = function(point) {
+	if(this.start === undefined){
+		this.start = point;
+	}
+	else{
+		this.end = point;
+	}
+}
+
+Line.prototype.draw = function(c) {
+	
+	c.beginPath();
+	c.moveTo(this.start.x,this.start.y);
+	c.lineTo(this.end.x, this.end.y);
+	c.stroke();
+}
+
+function Circle (point) {
+	
+	this.x = undefined;
+	this.y = undefined;
+}
+
+Circle.prototype.addPoint = function(point) {
+	if(this.start === undefined){
+		this.start = point;
+	}
+	else{
+		this.end = point;
+	}
+
+}
+Circle.prototype.draw = function(c) {
+	
+	var centerX = ((this.end.x + this.start.x)/2);
+	var centerY = ((this.end.y + this.start.y)/2);
+	var w = Math.abs(this.end.x - this.start.x);
+	var h = this.start.y - this.start.y;
+	var r = (w/2);
+	
+	c.arc(centerX,centerY,r,0,2*Math.PI,false);
+	c.stroke();
+	
+}
---
+++ skilaverkefni 1/Veff2/script.js
@@ -0,0 +1,106 @@
+$(function() {
+	
+	var canvas = document.getElementById("c");
+	var ctx = canvas.getContext("2d");
+	
+	var isDrawing = false;
+
+	var undo = [];
+	var shapes = [];
+	var currentTool = undefined;
+
+	// Pen = 0, Rectangle = 1
+	var currentToolType = 0;
+
+	$("#penTool").on("click", function() {
+		currentToolType = 0;
+		console.log("Selecting pen tool!");
+	})
+
+	$("#rectangleTool").on("click", function() {
+		currentToolType = 1;
+		console.log("Selecting rectangle tool!");
+	})
+	
+	$("#lineTool").on("click", function() {
+		currentToolType = 2;
+		console.log("Selecting line tool!");
+	})
+	
+	$("#circleTool").on("click", function() {
+		currentToolType = 3;
+		console.log("Selecting circle tool!");
+	})
+
+	function createNewTool() {
+		if(currentToolType === 0) {
+			return new Pencil();
+		}
+		else if(currentToolType === 1) {
+			return new Rect();
+		}
+		else if(currentToolType === 2) {
+			return new Line();
+		}
+		else if(currentToolType === 3) {
+			return new Circle();
+		}
+	}
+	
+	canvas.onmousedown = function(e) {
+		currentTool = createNewTool();
+	
+		isDrawing = true;
+		
+		var x = e.clientX - this.offsetLeft;
+		var y = e.clientY - this.offsetTop;
+		
+		var point = new Point(x, y);
+		currentTool.addPoint(point);
+	}
+	
+	canvas.onmousemove = function(e) {
+		if(isDrawing) {
+			var x = e.clientX - this.offsetLeft;
+			var y = e.clientY - this.offsetTop;
+			
+			var point = new Point(x, y);
+			currentTool.addPoint(point);
+		
+			//clearWindow();
+			drawShapes();
+			currentTool.draw(ctx);
+		}
+	}
+	
+	canvas.onmouseup = function(e) {
+		isDrawing = false;
+		shapes.push(currentTool);
+		console.log(shapes);
+	}
+	
+	function clearWindow() {
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		ctx.beginPath();
+	}
+	
+	function drawShapes() {
+		for(var i = 0; i < shapes.length; ++i) {
+			shapes[i].draw(ctx);
+		}
+	}
+	
+	$("#undo").on("click", function(e) {
+		shapes.pop();
+		clearWindow();
+		drawShapes();
+	});
+	
+	$("#clearButton").on("click", function(e) {
+		clearWindow();
+	});
+	
+	$("#drawShapes").on("click", function(e) {
+		drawShapes();
+	});
+});
