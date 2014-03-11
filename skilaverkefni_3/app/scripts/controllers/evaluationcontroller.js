66463e9792015115404388cb4c57de19997430ca
TheodorTomas <tomminn666@hotmail.com>
2014-03-11 18:47:09 +0000

    koma svo

--- skilaverkefni_3/app/scripts/controllers/evaluationcontroller.js
+++ skilaverkefni_3/app/scripts/controllers/evaluationcontroller.js
@@ -1,20 +1,46 @@
-var EvaluationCtrl = function($scope, modalInstance, $modal, items){
-	console.log('Creating a new evaluation!');
+app.controller('EvaluationController', [
+	"$scope", "EvalService", "$routeParams",
+	function($scope, EvalService, $routeParams) {
+		var evaluationID = $routeParams.evaluationID;
 
-	$scope.addQ = function(){
-	console.log('addQ');
-	$("#questions").load("app/qHeader.html");
+		if(evaluationID !== undefined) {
+			EvalService.getEvaluationById(evaluationID).then(function(data) {
+				$scope.evaluation = data;
+			}, function(errorMessage) {
+				console.log("Error fetching evaluation: " + errorMessage);
+			});
+		}
+		else {
+			$scope.evaluation = {
+				TitleIS: "",
+				TitleEN: "",
+				IntroTextIS: "",
+				IntroTextEN: "",
+				CourseQuestions: [],
+				TeacherQuestions: []
+			};
+		}
 
+		$scope.addQ = function(){
+			console.log("Hello from addQ");
+			var header = $("#qHeader").html();
+			$("#questions").append(header);
+
+		}
+
+		$scope.addAnswer = function(question) {
+			question.Answers.push("New answer");
+		}
+
+		$scope.addCourseQuestion = function() {
+			$scope.evaluation.CourseQuestions.push({
+				ID: $scope.evaluation.CourseQuestions.length,
+				TextIS: "",
+				TextEN: "",
+				ImageURL: "",
+				Type: "single",
+				Answers: []
+			});
+		}
 	}
-};
-
-$scope.finish = function(){
-
-
-};
-
-function addQ (){
-	console.log('addQ');
-	$("#questions").load("app/qHeader.html");
-
-	}
+]);
